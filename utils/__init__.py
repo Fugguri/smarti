c37f84a52f27b16b4906e9a4b44e38d5864b14a4
Fugguri <fygguri@gmail.com>
2024-04-03 18:19:53 +0300

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,7 +6,7 @@ from config import cfg
 from .SaleBotService import SalebotService
 # gpt_service = GPTService(api_key=cfg.tg_bot.openai)
 salebot = SalebotService()
-google = GoogleService()
+# google = GoogleService()
 try:
     assistant = AssistantService(api_key=cfg.tg_bot.openai)
 except Exception as ex:
